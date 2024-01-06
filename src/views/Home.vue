782b9ecc94ec2ab4c36e36e7246060430074d6db
Leon Chux <epicgenii18@gmail.com>
2024-01-05 20:09:14 -0400

    implimented rick and morty public api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,121 +1,143 @@
 <template>
-  <div class="justify-center max-w-md mt-10">
-    <h1 class="font-extrabold tracking-tigh text-3xl leading-10">
-      Ready for the challenge?
-    </h1>
-    <div class="mt-8">
-      <section>
-        <h2 class="font-bold text-2xl leading-6">Exercise 1</h2>
-        <div class="mt-5">
-          <p>
-            In the first exercise the goal would be to extend the current app
-            with some additional output. For this you should integrate an public
-            available Rest-API or GraphQL-API (see below for examples).
-          </p>
-          <p>
-            It's your own decision what kind of output you use and in which way
-            it will be displayed, but it should be in a creative way and you can
-            use this possibility to show us your existing skills. For example
-            you can add a list output as a starting point and show additional
-            information in a detail page (or other output types).
-          </p>
-          <br />
-          <p>Here are some examples for a public available API:</p>
-          <ul>
-            <li>
-              <a
-                class="text-blue-600 hover:text-blue-800 cursor-pointer"
-                href="https://github.com/APIs-guru/graphql-apis"
-                target="_blank"
-                >Public GraphQL-API Collection</a
+  <div class="w-full min-h-screen pt-10 pb-14 pl-2 pr-2">
+    <div v-if="!isLoadingApiCall && characters.length < 1" class="text-center">
+      <p>No characters to display</p>
+    </div>
+
+    <div class="flex justify-between rounded p-3 bg-red-400 w-60">
+      <div class="flex gap-x-4">
+        <div>Icon</div>
+        <div class="text-left">
+          <p>Info</p>
+          <p>Message Content</p>
+        </div>
+      </div>
+
+      <div>
+        <p>&times;</p>
+      </div>
+    </div>
+
+    <div class="mb-4">
+      <div
+        class="w-full lg:w-3/4 ml-auto mr-auto mb-2 flex justify-center items-stretch"
+      >
+        <input
+          v-model="nameFilter"
+          class="border-gray-400 border-2 w-3/4 md:w-2/4 rounded-r-none border-r-0 text-sm rounded-lg"
+          type="search"
+          placeholder=" Search by name"
+          v-on:change="handleInputChange"
+          v-on:keydown.enter="handleNameFilterSubmit"
+        />
+        <button
+          class="bg-gray-600 rounded-r-lg text-sm text-white pl-4 pr-4 pt-2 pb-2"
+          type="button"
+          v-on:click="getCharacters"
+        >
+          Search
+        </button>
+      </div>
+      <p class="text-center text-xs text-gray-500">
+        Clear the search field and click the search button to reset filter
+      </p>
+    </div>
+
+    <div v-if="isLoadingApiCall">Loading data...</div>
+
+    <div
+      v-if="!isLoadingApiCall"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
+    >
+      <div
+        v-for="(singleCharacter, index) in characters"
+        v-bind:key="index"
+        class="shadow-lg rounded-lg"
+      >
+        <div>
+          <img
+            v-bind:alt="singleCharacter.name"
+            v-bind:src="singleCharacter.image"
+            class="w-full h-auto"
+          />
+
+          <div class="text-left p-2 text-xs lg:text-sm">
+            <div class="mb-2">
+              <button
+                type="button"
+                class="text-2xl"
+                v-on:click="toggleFavorite(index)"
               >
-              (e.g.
-              <a
-                class="text-blue-600 hover:text-blue-800 cursor-pointer"
-                href="https://api.spacex.land/graphql/"
-                target="_blank"
-                >SpaceX-API</a
-              >)
-            </li>
-            <li>
-              <a
-                class="text-blue-600 hover:text-blue-800 cursor-pointer"
-                href="https://github.com/public-apis/public-apis"
-                target="_blank"
-                >Public REST-API Collection</a
-              >
-              (e.g.
-              <a
-                class="text-blue-600 hover:text-blue-800 cursor-pointer"
-                href="https://pokeapi.co/docs/v2"
-                target="_blank"
-                >Pokemon-API</a
-              >)
-            </li>
-          </ul>
-          -
+                <span v-if="favouriteCharacters[index]" class="text-red-600">
+                  &hearts;
+                </span>
+                <span
+                  v-if="!favouriteCharacters[index]"
+                  class="hover:text-red-600"
+                >
+                  &#x2661;
+                </span>
+              </button>
+            </div>
+            <h4 class="mb-2">Character Name - {{ singleCharacter.name }}</h4>
+            <p class="mb-2">Status - {{ singleCharacter.status }}</p>
+            <p class="mb-2">Species - {{ singleCharacter.species }}</p>
+            <p class="mb-2">Gender -{{ singleCharacter.gender }}</p>
+            <p class="mb-2">Origin - {{ singleCharacter.origin.name }}</p>
+            <p class="mb-2">Location - {{ singleCharacter.location.name }}</p>
+            <p class="mb-2">
+              No of Episodes Featured - {{ singleCharacter.episode.length }}
+            </p>
+          </div>
         </div>
-        <div class="mt-5">
-          <a
-            class="text-blue-600 hover:text-blue-800 cursor-pointer"
-            v-on:click="showNextExercise('second')"
-            >{{
-              showExercise.second
-                ? 'Hide the next exercise'
-                : 'Show the next exercise'
-            }}</a
-          >
-        </div>
-      </section>
-      <section v-if="showExercise.second" class="mt-10">
-        <h2 class="font-bold text-2xl leading-6">Exercise 2</h2>
-        <div class="mt-5">
-          <p>
-            As a second exercise you should add an global tost/notify handling
-            in the already extended app. Maybe you can do this in a easy way
-            with the new composition API.
-          </p>
-          <p>
-            For example you can show a toast after something was fetched from
-            the public API (which you implemented before) or also in error
-            situtations.
-          </p>
-        </div>
-        <div class="mt-5">
-          <a
-            class="text-blue-600 hover:text-blue-800 cursor-pointer"
-            v-on:click="showNextExercise('third')"
-            >{{
-              showExercise.third
-                ? 'Hide the next exercise'
-                : 'Show the next exercise'
-            }}</a
-          >
-        </div>
-      </section>
-      <section v-if="showExercise.third" class="mt-10">
-        <h2 class="font-bold text-2xl leading-6">Exercise 3</h2>
-        <div class="mt-5">
-          <p>
-            Please integrate a testing library in the current app. We're pretty
-            sure that you have some components which needs to be tested.
-          </p>
-        </div>
-      </section>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
-import { ShowExercises } from '../types/exercise'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { RickAndMortCharacterList } from '../types/favourites'
 
-const showExercise = reactive<ShowExercises>({
-  second: false,
-  third: false,
-})
+const characters = ref<RickAndMortCharacterList>([])
+const favouriteCharacters = ref<Array<boolean>>([])
+const isLoadingApiCall = ref(false)
+const nameFilter = ref('')
 
-const showNextExercise = (exercise: keyof ShowExercises) => {
-  showExercise[exercise] = !showExercise[exercise]
+const getCharacters = async () => {
+  isLoadingApiCall.value = true
+  try {
+    const response = await axios.get(
+      `https://rickandmortyapi.com/api/character?name=${nameFilter.value}`,
+    )
+    characters.value = response.data.results
+  } catch (error: any) {
+    console.log(error)
+
+  }
+  isLoadingApiCall.value = false
 }
+
+const toggleFavorite = (characterIndex: number) => {
+  const favoritesClone = [...favouriteCharacters.value]
+  if (favoritesClone[characterIndex]) {
+    delete favoritesClone[characterIndex]
+  } else {
+    favoritesClone[characterIndex] = true
+  }
+  favouriteCharacters.value = favoritesClone
+}
+const handleInputChange = (event: Event) => {
+  nameFilter.value = (event.target as HTMLInputElement).value
+}
+const handleNameFilterSubmit = (event: KeyboardEvent) => {
+  if (event.key === 'Enter') {
+    getCharacters()
+  }
+}
+
+onMounted(() => {
+  getCharacters()
+})
 </script>
